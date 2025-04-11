d01c90323a78bc53d610e8f7920e14130f089caa
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-04-11 09:47:51 +0200

    chore: 🧑‍💻  enhance devcontainer setup with Apache, Supervisor, and PHP configurations
    Give access to:
    
    -  <http://symfony-playground.localhost:forwarded-port>
    -  <http://pgadmin.symfony-playground.localhost:forwarded-port>
    -  <http://mailpit.symfony-playground.localhost:forwarded-port>

--- .devcontainer/bin/postCreateCommand.sh
+++ .devcontainer/bin/postCreateCommand.sh
@@ -6,8 +6,27 @@ TOOLS_FOLDER="${WORKSPACE_FOLDER}/tools"
 
 function add-to-path() {
   [[ ${#} -ne 1 ]] && echo "Usage: add-to-path <path>" && return 1
-  local path = "${1}"
-  local vendor_path = "${path}/vendor/bin"
+  local path="${1}"
+
+  # Check if path exists
+  [[ -n "${path}" || -d "${path}" ]] || return 1
+
+  # Check if vendor_path is already in PATH
+  [[ ":${PATH}:" != *":${path}:"* ]] || return 0
+
+  # Add path to PATH in user profile and bashrc
+  echo "PATH=\"${path}:\$PATH\"" >>"${HOME}/.profile"
+  echo "PATH=\"${path}:\$PATH\"" >>"${HOME}/.bashrc"
+
+  # Add vendor_path to PATH
+  export PATH="${path}:${PATH}"
+}
+
+
+function add-vendor-bin-to-path() {
+  [[ ${#} -ne 1 ]] && echo "Usage: add-vendor-bin-to-path <path>" && return 1
+  local path="${1}"
+  local vendor_path="${path}/vendor/bin"
 
   # Check if path exists
   [[ -n "${path}" || -d "${path}" ]] || return 1
@@ -15,25 +34,22 @@ function add-to-path() {
   # Check if vendor_path exists
   [[ ! -d "${vendor_path}" ]] && echo "No vendor/bin directory found in '${path}'" && return 1
 
-  # Check if vendor_path is already in PATH
-  [[ ":${PATH}:" != *":${vendor_path}:"* ]] || return 0
-
-  # Add path to PATH in user profile
-	echo "PATH=\"${path}:\$PATH\"" >> "${HOME}/.profile"
-
- # Add vendor_path to PATH
-	export PATH="${vendor_path}:${PATH}"
+  # add to path
+  add-to-path "${vendor_path}" || return 1
 }
 
+
+
 # Add tools to PATH
-add-to-path "${TOOLS_FOLDER}/phpstan"
-add-to-path "${TOOLS_FOLDER}/psalm"
-add-to-path "${TOOLS_FOLDER}/rector"
-add-to-path "${TOOLS_FOLDER}/php-cs-fixer"
-add-to-path "${TOOLS_FOLDER}/phpcs"
-add-to-path "${TOOLS_FOLDER}/phpmd"
+add-vendor-bin-to-path "${TOOLS_FOLDER}/phpstan"
+add-vendor-bin-to-path "${TOOLS_FOLDER}/psalm"
+add-vendor-bin-to-path "${TOOLS_FOLDER}/rector"
+add-vendor-bin-to-path "${TOOLS_FOLDER}/php-cs-fixer"
+add-vendor-bin-to-path "${TOOLS_FOLDER}/phpcs"
+add-vendor-bin-to-path "${TOOLS_FOLDER}/phpmd"
 
 # Add project bin to PATH
+add-vendor-bin-to-path "${WORKSPACE_FOLDER}"
 add-to-path "${WORKSPACE_FOLDER}/bin"
 
 # Clear cache and warmup
@@ -44,7 +60,7 @@ php bin/console cache:warmup
 php bin/console doctrine:database:create --if-not-exists
 
 # Run migrations
-php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 # Install nvm
 # nvm install --lts
