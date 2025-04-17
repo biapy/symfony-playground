e9f23b75c934e528ba556496d97409388e38f8f8
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-04-17 08:38:12 +0200

    feat: 🏗️ add BlogPostModel with validation constraints and AutoMapper integration

---
+++ src/Form/Model/BlogPostModel.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright 2025 Biapy
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+namespace App\Form\Model;
+
+use App\Entity\BlogPost;
+use AutoMapper\Attribute\Mapper;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[Mapper(source: BlogPost::class, target: BlogPost::class, dateTimeFormat: 'Y-m-d H:i:s')]
+class BlogPostModel
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Length(max: 255)]
+        public string $title,
+
+        #[Assert\NotBlank]
+        public string $content,
+
+        #[Assert\DateTime]
+        public ?string $published = null,
+    ) {
+    }
+}
