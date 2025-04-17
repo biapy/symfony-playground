c05197d1d3e1b9378f62a438bf41fc18e6cc31cc
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-04-17 08:37:53 +0200

    build: ➕ add AutoMapperBundle and configure lock settings in Symfony

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    AutoMapper\Symfony\Bundle\AutoMapperBundle::class => ['all' => true],
 ];
