f49167d730ba8335a567e7b73812e27877be1463
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-04-18 08:29:41 +0200

    feat: 🗃️ add BlogPost fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,7 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * @copyright 2025 Biapy
+ * @license MIT
+ */
+
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +19,22 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $blogPost = new BlogPost(
+            title: 'A first post!',
+            content: 'Post text!',
+        );
+        $blogPost->setPublished(new \DateTimeImmutable('2025-04-01 12:00:00'));
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost(
+            title: 'A second post!',
+            content: 'Post text!',
+        );
+        $blogPost->setPublished(new \DateTimeImmutable('2025-04-02 12:00:00'));
+
+        $manager->persist($blogPost);
 
         $manager->flush();
     }
