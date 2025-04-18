b9978ef71180b34f0dfdd8e65a5292825878bb8c
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-04-18 08:55:55 +0200

    feat: ✨ add BlogPostCrudController and DashboardController for managing blog posts

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @copyright 2025 Biapy
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Entity\BlogPost;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+final class DashboardController extends AbstractDashboardController
+{
+    #[\Override]
+    public function index(): Response
+    {
+        // return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // 1.1) If you have enabled the "pretty URLs" feature:
+        return $this->redirectToRoute('admin_blog_post_index');
+        //
+        // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirectToRoute('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    #[\Override]
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Symfony Playground');
+    }
+
+    #[\Override]
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Blog', 'fas fa-blog');
+        yield MenuItem::linkToCrud('Blog Posts', 'fas fa-pen-fancy', BlogPost::class);
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
