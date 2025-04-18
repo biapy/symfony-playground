d05ac695453844fc6511d96a45f2b685b490b5b1
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-04-18 08:55:55 +0200

    feat: ✨ add BlogPostCrudController and DashboardController for managing blog posts

---
+++ src/Controller/Admin/BlogPostCrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @copyright 2025 Biapy
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Entity\BlogPost;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+final class BlogPostCrudController extends AbstractCrudController
+{
+    #[\Override]
+    public static function getEntityFqcn(): string
+    {
+        return BlogPost::class;
+    }
+
+    #[\Override]
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('title'),
+            TextEditorField::new('content'),
+            DateTimeField::new('published')
+                ->setFormat('dd/MM/yyyy HH:mm:ss')
+                ->setHelp('Date and time of publication'),
+        ];
+    }
+}
