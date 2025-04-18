0e7fa556a29a2fee3b7dad621ca65fc51535e34c
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-04-18 08:23:58 +0200

    build: ➕ add DoctrineFixturesBundle and AppFixtures for data loading in development and testing

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     AutoMapper\Symfony\Bundle\AutoMapperBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
