0d19a5ae85d2d41983d26ed1b6917052786a290d
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-05-07 12:15:48 +0200

    feat: ✨ associate blog posts with the anonymous user in AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,20 +10,33 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Nepada\EmailAddress\RfcEmailAddress;
 
 final class AppFixtures extends Fixture
 {
+    /**
+     * {@inheritDoc}
+     *
+     * @SuppressWarnings("PHPMD.StaticAccess")
+     */
     #[\Override]
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $anonymousUser = new User(
+            email: RfcEmailAddress::fromString('none@none.localhost'),
+            passwordHash: '',
+        );
+        $anonymousUser->setDisplayName('None');
+
+        $manager->persist($anonymousUser);
 
         $blogPost = new BlogPost(
             title: 'A first post!',
             content: 'Post text!',
+            author: $anonymousUser,
         );
         $blogPost->setPublished(new \DateTimeImmutable('2025-04-01 12:00:00'));
 
@@ -32,6 +45,7 @@ final class AppFixtures extends Fixture
         $blogPost = new BlogPost(
             title: 'A second post!',
             content: 'Post text!',
+            author: $anonymousUser,
         );
         $blogPost->setPublished(new \DateTimeImmutable('2025-04-02 12:00:00'));
 
