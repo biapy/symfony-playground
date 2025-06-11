66f78d2bdde0cbeeb58714e88369e15327d53765
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-06-11 08:57:27 +0200

    refactor: ♻️ Restructure AppFixtures to separate user and blog post loading logic

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,9 +14,15 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Nepada\EmailAddress\RfcEmailAddress;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class AppFixtures extends Fixture
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
     /**
      * {@inheritDoc}
      *
@@ -25,13 +31,14 @@ final class AppFixtures extends Fixture
     #[\Override]
     public function load(ObjectManager $manager): void
     {
-        $anonymousUser = new User(
-            email: RfcEmailAddress::fromString('none@none.localhost'),
-            passwordHash: '',
-        );
-        $anonymousUser->setDisplayName('None');
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+        $manager->flush();
+    }
 
-        $manager->persist($anonymousUser);
+    private function loadBlogPosts(ObjectManager $manager): void
+    {
+        $anonymousUser = $this->getReference('anonymous_user', User::class);
 
         $blogPost = new BlogPost(
             title: 'A first post!',
@@ -50,7 +57,39 @@ final class AppFixtures extends Fixture
         $blogPost->setPublished(new \DateTimeImmutable('2025-04-02 12:00:00'));
 
         $manager->persist($blogPost);
+    }
 
-        $manager->flush();
+    private function loadUsers(ObjectManager $manager): void
+    {
+        $anonymousUser = new User(
+            email: $this->createRfcEmailAddressFromString('none@none.localhost'),
+        );
+        $anonymousUser->setDisplayName('None');
+
+        $this->addReference('anonymous_user', $anonymousUser);
+
+        $manager->persist($anonymousUser);
+
+        $adminUser = new User(
+            email: $this->createRfcEmailAddressFromString('admin@blog.com'),
+        );
+        $passwordHash = $this->passwordHasher->hashPassword(
+            $adminUser,
+            'admin@blog.com',
+        );
+        $adminUser->setPasswordHash($passwordHash);
+        $adminUser->setDisplayName('Admin');
+
+        $this->addReference('admin_user', $adminUser);
+
+        $manager->persist($adminUser);
+    }
+
+    /**
+     * @SuppressWarnings("PHPMD.StaticAccess")
+     */
+    private function createRfcEmailAddressFromString(string $email): RfcEmailAddress
+    {
+        return RfcEmailAddress::fromString($email);
     }
 }
