f81efc55c139e9a215290fb59467fdd71968dfda
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-06-12 08:17:27 +0200

    build: 🧑‍💻 update tools folder path to vendor-bin in postCreateCommand.sh

--- .devcontainer/bin/postCreateCommand.sh
+++ .devcontainer/bin/postCreateCommand.sh
@@ -2,43 +2,40 @@
 
 WORKSPACE_FOLDER="$(command pwd)"
 
-TOOLS_FOLDER="${WORKSPACE_FOLDER}/tools"
+TOOLS_FOLDER="${WORKSPACE_FOLDER}/vendor-bin"
 
 function add-to-path() {
-  [[ ${#} -ne 1 ]] && echo "Usage: add-to-path <path>" && return 1
-  local path="${1}"
+    [[ ${#} -ne 1 ]] && echo "Usage: add-to-path <path>" && return 1
+    local path="${1}"
 
-  # Check if path exists
-  [[ -n "${path}" || -d "${path}" ]] || return 1
+    # Check if path exists
+    [[ -n "${path}" || -d "${path}" ]] || return 1
 
-  # Check if vendor_path is already in PATH
-  [[ ":${PATH}:" != *":${path}:"* ]] || return 0
+    # Check if vendor_path is already in PATH
+    [[ ":${PATH}:" != *":${path}:"* ]] || return 0
 
-  # Add path to PATH in user profile and bashrc
-  echo "PATH=\"${path}:\$PATH\"" >>"${HOME}/.profile"
-  echo "PATH=\"${path}:\$PATH\"" >>"${HOME}/.bashrc"
+    # Add path to PATH in user profile and bashrc
+    echo "PATH=\"${path}:\$PATH\"" >>"${HOME}/.profile"
+    echo "PATH=\"${path}:\$PATH\"" >>"${HOME}/.bashrc"
 
-  # Add vendor_path to PATH
-  export PATH="${path}:${PATH}"
+    # Add vendor_path to PATH
+    export PATH="${path}:${PATH}"
 }
-
 
 function add-vendor-bin-to-path() {
-  [[ ${#} -ne 1 ]] && echo "Usage: add-vendor-bin-to-path <path>" && return 1
-  local path="${1}"
-  local vendor_path="${path}/vendor/bin"
+    [[ ${#} -ne 1 ]] && echo "Usage: add-vendor-bin-to-path <path>" && return 1
+    local path="${1}"
+    local vendor_path="${path}/vendor/bin"
 
-  # Check if path exists
-  [[ -n "${path}" || -d "${path}" ]] || return 1
+    # Check if path exists
+    [[ -n "${path}" || -d "${path}" ]] || return 1
 
-  # Check if vendor_path exists
-  [[ ! -d "${vendor_path}" ]] && echo "No vendor/bin directory found in '${path}'" && return 1
+    # Check if vendor_path exists
+    [[ ! -d "${vendor_path}" ]] && echo "No vendor/bin directory found in '${path}'" && return 1
 
-  # add to path
-  add-to-path "${vendor_path}" || return 1
+    # add to path
+    add-to-path "${vendor_path}" || return 1
 }
-
-
 
 # Add tools to PATH
 add-vendor-bin-to-path "${TOOLS_FOLDER}/phpstan"
