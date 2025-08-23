817f4ccf68494ba64c25abce5e8bb280235950ad
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-08-24 00:46:44 +0200

    feat(composer): :heavy_plus_sign: add orm-fixtures bundle to project

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @copyright 2025 Biapy
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
