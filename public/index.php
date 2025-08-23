c576c22cb5483555e483f16ccbe8c3eca3cd72eb
Pierre-Yves Landuré <py.landure@cobredia.fr>
2025-08-24 00:31:00 +0200

    fix: :rotating_light: fix PHPStan warning on ['APP_ENV'] type

--- public/index.php
+++ public/index.php
@@ -1,9 +1,20 @@
 <?php
 
+/**
+ * @copyright 2025 Biapy
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    if (!is_string($context['APP_ENV'])) {
+        throw new InvalidArgumentException('APP_ENV must be a string');
+    }
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
